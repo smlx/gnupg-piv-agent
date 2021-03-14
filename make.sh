c23c68d25ea62cdcc0976336e5fc8cd34db29cdb
Scott Leggett <scott@sl.id.au>
2021-03-14 22:05:57 +0800

    chore: Initial import

---
+++ make.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env sh
+
+prefix=$(pwd)/dest
+
+#tar xf npth-*.tar.bz2
+(
+  cd npth-* || exit
+  ./configure --prefix="$prefix"
+  make -j 8
+  make install
+)
+#tar xf libgpg-error-*.tar.bz2
+(
+  cd libgpg-error-* || exit
+  ./configure --prefix="$prefix"
+  make -j 8
+  make install
+)
+#tar xf libgcrypt-*.tar.bz2
+(
+  cd libgcrypt-* || exit
+  ./configure --prefix="$prefix" --with-libgpg-error-prefix="$prefix"
+  make -j 8
+  make install
+)
+#tar xf libksba-*.tar.bz2
+(
+  cd libksba-* || exit
+  ./configure --prefix="$prefix" --with-libgpg-error-prefix="$prefix"
+  make -j 8
+  make install
+)
+#tar xf libassuan-*.tar.bz2
+(
+  cd libassuan-* || exit
+  ./configure --prefix="$prefix" --with-libgpg-error-prefix="$prefix"
+  make -j 8
+  make install
+)
+#tar xf gnupg-*.tar.bz2
+(
+  cd gnupg-* || exit
+  ./configure \
+    --prefix="$prefix" \
+    --with-npth-prefix="$prefix" \
+    --with-libgpg-error-prefix="$prefix" \
+    --with-libgcrypt-prefix="$prefix" \
+    --with-libassuan-prefix="$prefix" \
+    --with-ksba-prefix="$prefix" \
+    LDFLAGS="-Wl,--rpath=$prefix/lib" \
+  ;
+  make -j 8
+  make install
+)
